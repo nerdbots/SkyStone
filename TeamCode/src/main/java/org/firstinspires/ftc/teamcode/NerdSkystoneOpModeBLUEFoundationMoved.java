6300a9ad706b03c0e69ff981dcfbf3c91757e26f
learnark1528 <hgomam@gmail.com>
2019-12-01 20:49:17 -0500

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NerdSkystoneOpModeBLUEFoundationMoved.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NerdSkystoneOpModeBLUEFoundationMoved.java
@@ -46,7 +46,7 @@ import java.util.HashMap;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@Autonomous(name="NerdSkystoneOpModeBLUEFoundationMoved", group="Linear Opmode")
+@Autonomous(name="NerdSkystoneOpMode_BLUE_FoundationMoved", group="Linear Opmode")
 //@Disabled
 public class NerdSkystoneOpModeBLUEFoundationMoved extends LinearOpMode {
     private NerdBOT myNerdBOT;
